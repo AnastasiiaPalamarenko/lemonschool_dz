4791fd75be8f35460cb62c6068c0e31c8be31066
Anastasiia Palamarenko <nastya.palamarenko02@gmail.com>
2021-03-30 23:49:56 +0300

    dz1

---
+++ dz1.js
@@ -0,0 +1,77 @@
+/*let number = prompt("Введите число от 0 до 9:");
+
+switch (number) {
+    case "0": console.log("ноль"); break;
+    case "1": console.log("один"); break;
+    case "2": console.log("два"); break;
+    case "3": console.log("три"); break;
+    case "4": console.log("четыре"); break;
+    case "5": console.log("пять"); break;
+    case "6": console.log("шесть"); break;
+    case "7": console.log("семь"); break;
+    case "8": console.log("восемь"); break;
+    case "9": console.log("девять"); break;
+    default: console.log("нет таких значений");
+}
+
+
+let number = prompt("Введите число от 0 до 9:");
+if (number == 0) {
+    console.log("ноль");
+} else if (number == 1) {
+    console.log("один");
+} else if (number == 2) {
+    console.log("два");
+} else if (number == 3) {
+    console.log("три");
+} else if (number == 4) {
+    console.log("четыре");
+} else if (number == 5) {
+    console.log("пять");
+} else if (number == 6) {
+    console.log("шесть");
+} else if (number == 7) {
+    console.log("семь");
+} else if (number == 8) {
+    console.log("восемь");
+} else if (number == 9) {
+    console.log("девять");
+};
+
+let number=prompt("Введите число:");
+if (number==0) {
+    console.log("Ваше число равно 0")
+} else if (number>0){
+    console.log("Ваше число положительное")
+} else if (number<0){
+    console.log("Ваше число отрицательное")
+};
+
+
+
+
+let unit = prompt("Введите еденицу измерения:");
+let number= prompt("Введите целое число:");
+if(unit==" Byte"){
+    console.log(number+unit)
+}else if(unit=="KB"){
+    console.log(number*1024+' Byte')
+}else if(unit=="MB"){
+    console.log(number*Math.pow(1024,2)+' Byte')
+}else if(unit=="GB"){
+    console.log(number*Math.pow(1024,3)+' Byte')
+};
+
+
+
+
+let percent=5;
+let credit=50000;
+let years=10;
+let percentAll=credit/100*percent*10;
+console.log(percentAll +' процентов заплатит клиент за '+ years + ' лет');
+let percentYear=credit/100*percent;
+console.log(percentYear +' процентов заплатит клиент за один год');
+let allMoney=percentAll+credit;
+console.log(allMoney +' клиент выплатит за все года');
+*/
